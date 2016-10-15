b85f1279fbf61221dfc452c4af2a9f9acd924b49
emad3020 <askerasker2016@gmail.com>
2016-10-15 10:51:54 +0200

    login activity added

---
+++ app/src/main/java/com/askerlap/emad/elsory/Activites/MainActivity.java
@@ -0,0 +1,15 @@
+package com.askerlap.emad.elsory.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.askerlap.emad.elsory.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/askerlap/emad/elsory/UI/LoginActivity.java
+++ app/src/main/java/com/askerlap/emad/elsory/UI/LoginActivity.java
@@ -1,15 +1,29 @@
 package com.askerlap.emad.elsory.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.askerlap.emad.elsory.Activites.MainActivity;
 import com.askerlap.emad.elsory.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        assert getSupportActionBar() !=null;
+        getSupportActionBar().hide();
+        login=(Button)findViewById(R.id.btn_account_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
